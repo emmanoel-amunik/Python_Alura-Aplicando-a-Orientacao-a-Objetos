aa0540d62ac14334f2002cb9344236d4a93386ee
emmanoel-amunik <emmanoelamunik@hotmail.com>
2024-02-16 19:21:37 -0300

    02. Construtor e instanciando objetos

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,16 +1,22 @@
 class Restaurant:
+    restaurants = []
 
-    name = ""
-    category = ""
-    activate = False
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.activate = False
+        Restaurant.restaurants.append(self)
+
+    def __str__(self):
+        return f"{self.name} | {self.category}"
+
+    @staticmethod
+    def list_restaurants():
+
+        for restaurant in Restaurant.restaurants:
+            print(f"{restaurant.name} | {restaurant.category} | "
+                  f"{restaurant.activate}")
 
 
-restaurant_square = Restaurant()
-restaurant_square.name = "Square"
-restaurant_square.category = "Gourmet"
-
-restaurant_pizza = Restaurant()
-
-restaurants = [restaurant_square, restaurant_pizza]
-
-print(restaurant_square)
+restaurant_square = Restaurant("Square", "Gourmet")
+restaurant_pizza = Restaurant("Pizza Express", "Italian")
