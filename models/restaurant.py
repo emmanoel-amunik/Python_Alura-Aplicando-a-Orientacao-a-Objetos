0b674fb5ef9d3c633cff0ddb00ff5e48f9f2b919
emmanoel-amunik <emmanoelamunik@hotmail.com>
2024-02-17 11:47:09 -0300

    03. Porperty e metodos de classe

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,22 +1,42 @@
 class Restaurant:
+
     restaurants = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.activate = False
+        self._name = name.title()
+        self._category = category.upper()
+        self._activate = False
         Restaurant.restaurants.append(self)
 
     def __str__(self):
-        return f"{self.name} | {self.category}"
+        return f"{self._name} | {self._category}"
 
-    @staticmethod
-    def list_restaurants():
+    def get_name(self):
+        return self._name
 
-        for restaurant in Restaurant.restaurants:
-            print(f"{restaurant.name} | {restaurant.category} | "
+    def get_category(self):
+        return self._category
+
+    @classmethod
+    def list_restaurants(cls):
+
+        print(f"{'Name'.ljust(27)} {'Category'.ljust(27)} {'Status'}\n")
+
+        for restaurant in cls.restaurants:
+            print(f"{restaurant.get_name().ljust(25)} | "
+                  f"{restaurant.get_category().ljust(25)} | "
                   f"{restaurant.activate}")
 
+    @property
+    def activate(self):
+        return "true" if self._activate else "false"
 
-restaurant_square = Restaurant("Square", "Gourmet")
-restaurant_pizza = Restaurant("Pizza Express", "Italian")
+    def change_status(self):
+        self._activate = not self._activate
+
+
+restaurant_square = Restaurant("square", "Gourmet")
+restaurant_pizza = Restaurant("pizza Express", "Italian")
+
+Restaurant.change_status(restaurant_square)
+Restaurant.list_restaurants()
